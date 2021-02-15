f3255863f1ba5c046aa14f6efdac2a0d55162065
yapasupa <eslhafkppmax@yahoo.co.jp>
2021-02-15 17:14:08 +0900

    商品情報編集機能コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,18 @@ def show
   @item =Item.find(params[:id])
 end
 
+def edit
+  @item =Item.find(params[:id])
+end
+
+def update
+  @item =Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to item_path
+  else
+    render :new
+end
+end
 
 private
 
